b2a297cdcee5df9e7d5484240ed348c6eb3a4c03
Miguel Fraga <mapena.16@est.ucab.edu.ve>
2019-06-14 20:50:40 -0400

    CRUD empleados funcional con base de datos

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -14,7 +14,7 @@ const { Client } = require('pg');  //Requerimos el paquete postgres
 const connectionData = {
   user: 'postgres',
   host: 'localhost',
-  database: 'loginTest',
+  database: 'proyectoBD1',
   password: 'admin',
   port: 5432,
 }; //En esta parte solo se modifica el 'user', 'database' y 'password' (Esta sección cambia por cada computadora y su config en BD)
@@ -33,12 +33,13 @@ app.set("view engine","ejs");
 
 //-----Login MinerUCAB-------
 
-//Lógica login
+
+//METODO GET
+
 app.get("/login",function(req,res){
   res.render('login');
-});
+}); 
 
-//Lógica de home 
 app.get("/Home",function(req,res){
   if(userJSON.usuario != "none"){
     res.render('home',{user: userJSON});
@@ -46,36 +47,6 @@ app.get("/Home",function(req,res){
     res.redirect('login');
   }
 });
-
-app.post('/login',function(req,res){
-  var userCheck = req.body.user;
-  var userPassword = req.body.password;
-  client.query('SELECT nombre,apellido,keyword FROM usuarios WHERE usuario = $1',[userCheck],(err,result)=>{
-      if(result.rows[0] != null && result.rows[1] == null){
-        if (result.rows[0].keyword == userPassword){
-          userJSON.nombre = result.rows[0].nombre;
-          userJSON.apellido = result.rows[0].apellido;
-          userJSON.usuario = userCheck;
-          userJSON.password = userPassword;
-          res.json('access');  
-        }else{
-          res.json('notAccess');
-        }
-      }else{
-        res.json('notAccess');
-      }
-  });
-});
-
-app.post("/logOut",function(req,res){
-  userJSON.nombre = "none";
-  userJSON.apellido = "none";
-  userJSON.usuario = "none"; 
-  userJSON.password = "none";
-  res.redirect('login');
-});
-
-//Logica home
 
 app.get("/Caja",function(req,res){
   if(userJSON.usuario != "none"){
@@ -117,40 +88,169 @@ app.get("/Empleados",function(req,res){
   }
 });
 
-
-
-
-
-
-//Test para insertar elemento en base de datos con formulario
-app.post("/users",function(req,res){
-  var nombre = req.body.nombre;
-  var apellido = req.body.apellido;
-  var edad = req.body.edad;
-  var usuario = req.body.usuario;
-  var keyword = req.body.keyword;
-  console.log("Nombre: " + nombre);
-  console.log("Apellido: " + apellido);
-  console.log("Edad: " + edad);
-  console.log("Usuario: " + usuario);
-  console.log("Keyword: " + keyword);
-  var query = "INSERT INTO USUARIOS (nombre,apellido,edad,usuario,keyword) VALUES ($1,$2,$3,$4,$5)";
-  var values = [nombre,apellido,edad,usuario,keyword];
-  genericInsertQuery(query,values);
-  res.render("users");
+app.get("/Empleados-Agregar",function(req,res){
+  if(userJSON.usuario != "none"){
+    res.render('empleadosAgregar',{user: userJSON});
+  }else{
+    res.redirect('login');
+  }
 });
 
-function genericInsertQuery(query,values){
-  client.query(query, values, (err, res) => {
+app.get("/Empleados-Consultar",function(req,res){
+  if(userJSON.usuario != "none"){
+    client.query('SELECT emp_cedula,emp_nombre1,emp_apellido1,emp_genero,emp_telefono FROM empleado',(err,result)=>{
+      if (err) {
+        console.log(err.stack);
+        res.send('failed'); 
+      }else if(result.rows[0] != null){
+        console.log(result.rows);
+        res.render('empleadosConsultar',{dataTable: result.rows, user: userJSON});
+      }else{
+        res.send('failed');
+      };
+    });
+  }else{
+    res.redirect('login');
+  }
+});
+
+app.get("/Empleados-Modificar",function(req,res){
+  if(userJSON.usuario != "none"){
+    res.render('empleadosModificar',{user: userJSON});
+  }else{
+    res.redirect('login');
+  }
+});
+
+app.get("/Empleados-Eliminar",function(req,res){
+  if(userJSON.usuario != "none"){
+    res.render('empleadosEliminar',{user: userJSON});
+  }else{
+    res.redirect('login');
+  }
+});
+
+
+
+//METODOS POST
+
+//URL para login
+app.post('/login',function(req,res){
+  var userCheck = req.body.user;
+  var userPassword = req.body.password;
+  client.query('SELECT E.emp_nombre1,E.emp_apellido1,U.usu_password FROM empleado AS E, usuario AS U WHERE U.fk_empleado = E.id_empleado AND U.usu_usuario = $1',[userCheck],(err,result)=>{
+      if(result.rows[0] != null && result.rows[1] == null){
+        if (result.rows[0].usu_password == userPassword){
+          userJSON.nombre = result.rows[0].emp_nombre1;
+          userJSON.apellido = result.rows[0].emp_apellido1;
+          userJSON.usuario = userCheck;
+          userJSON.password = userPassword;
+          res.send('access');  
+        }else{
+          res.send('notAccess');
+        }
+      }else{
+        res.send('notAccess');
+      }
+  });
+});
+
+app.post("/logOut",function(req,res){
+  userJSON.nombre = "none";
+  userJSON.apellido = "none";
+  userJSON.usuario = "none"; 
+  userJSON.password = "none";
+  res.redirect('login');
+});
+
+//URL para agregar empleado
+app.post("/Empleados-Agregar",function(req,res){
+  var empleadoPrimerNombre = req.body.pnombre;
+  var empleadoSegundoNombre = req.body.snombre;
+  var empleadoPrimerApellido = req.body.papellido;
+  var empleadoSegundoApellido = req.body.sapellido;
+  var empleadoCedula = req.body.cedula;
+  var empleadoNacionalidad = req.body.nacionalidad;
+  var empleadoGenero = req.body.genero;
+  var empleadoFechaNacimiento = req.body.fnac;
+  var empleadoTelefono = req.body.telefono;
+
+  if(empleadoGenero == 'Femenino'){
+    empleadoGenero = 'F';
+  }else if(empleadoGenero == 'Masculino'){
+    empleadoGenero = 'M';
+  }else{
+    empleadoGenero = 'O';
+  }
+
+  client.query('INSERT INTO empleado (emp_cedula,emp_nacionalidad,emp_nombre1,emp_nombre2,emp_apellido1,emp_apellido2,emp_fechanacimiento,emp_genero,emp_telefono) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)',[empleadoCedula,empleadoNacionalidad,empleadoPrimerNombre,empleadoSegundoNombre,empleadoPrimerApellido,empleadoSegundoApellido,empleadoFechaNacimiento,empleadoGenero,empleadoTelefono],(err,result)=>{
     if (err) {
       console.log(err.stack);
-      client.end();
-    } else {
+      res.send('failed'); 
+    }else{
+      res.send('great'); 
       console.log('Query procesado correctamente');
-      client.end();
+    };
+  });
+});
+
+//URL para eliminar empleados
+app.post("/Empleados-Eliminar",function(req,res){ 
+  var cedulaEliminar = req.body.cedulaEmp;
+  var nacionalidad = req.body.nacionalidadEmp;
+  client.query('DELETE FROM empleado AS E WHERE E.emp_cedula = $1 AND E.emp_nacionalidad = $2',[cedulaEliminar,nacionalidad],(err,result)=>{
+    if (err) {
+      console.log(err.stack);
+      res.send('failed'); 
+    }else{
+      res.send('great');
     }
   });
-};
+});
+
+//URL para verificar empleado en modificacion
+app.post("/Empleados-Verificar",function(req,res){ 
+  var cedulaV = req.body.cedulaEmpV;
+  var nacionalidadV = req.body.nacionalidadV;
+  client.query('SELECT emp_cedula,emp_nacionalidad,emp_nombre1,emp_nombre2,emp_apellido1,emp_apellido2,emp_fechanacimiento,emp_genero,emp_telefono FROM empleado WHERE emp_cedula = $1 AND emp_nacionalidad = $2',[cedulaV,nacionalidadV],(err,result)=>{
+    if (err) {
+      console.log(err.stack);
+      res.send('failed'); 
+    } else if(result.rows[0] != null){
+      var dataV = result.rows;
+      res.json(dataV);
+    }else{
+      console.log('Entra aqui');
+      res.send('failed');
+    }
+  });
+});
+
+app.post("/Empleados-Modificar",function(req,res){ 
+  var empleadoPrimerNombre = req.body.pnombreGC;
+  var empleadoSegundoNombre = req.body.snombreGC;
+  var empleadoPrimerApellido = req.body.papellidoGC;
+  var empleadoSegundoApellido = req.body.sapellidoGC;
+  var empleadoCedula = req.body.cedulaGC;
+  var empleadoNacionalidad = req.body.nacionalidadGC;
+  var empleadoGenero = req.body.generoGC;
+  var empleadoFechaNacimiento = req.body.fnacGC;
+  var empleadoTelefono = req.body.telefonoGC;
+
+  console.log(empleadoGenero);
+
+  client.query('UPDATE EMPLEADO SET emp_nacionalidad=$1,emp_nombre1=$2,emp_nombre2=$3,emp_apellido1=$4,emp_apellido2=$5,emp_fechanacimiento=$6,emp_genero= $7,emp_telefono= $8 WHERE emp_cedula=$9',[empleadoNacionalidad,empleadoPrimerNombre,empleadoSegundoNombre,empleadoPrimerApellido,empleadoSegundoApellido,empleadoFechaNacimiento,empleadoGenero,empleadoTelefono,empleadoCedula],(err,result)=>{
+    if (err) {
+      console.log(err.stack);
+      res.send('failed'); 
+    }else{
+      res.send('great'); 
+      console.log('Query procesado correctamente');
+    };
+  });
+});
+
+//URL para modificar el empleado verificado
 
 //Puerto donde se escuchan las peticiones http
 app.listen(8080);
--- Proyecto/public/js/logic.js
+++ Proyecto/public/js/logic.js
@@ -1,20 +1,165 @@
-//Login
-$('#userLogin').focus(function(){
-	$('#userLogin').removeClass('formsFieldError');
+
+//PETICIONES AJAX
+
+$('#agregarEmpleado').on('submit',function(e){
+	e.preventDefault();
+	let pNombre = $('#nombreEmpleado');
+	let sNombre = $('#segundoNombreEmpleado');
+	let pApellido = $('#apellidoEmpleado');
+	let sApellido = $('#segundoApellidoEmpleado');
+	let cedula = $('#cedulaEmpleado');
+	let nacionalidad = $('#nacionalidadEmpleado');
+	let telefono = $('#telefonoEmpleado');
+	let genero = $('#generoEmpleado');
+	let fnac = $('#fechaNacimientoEmpleado');
+
+	$.ajax({
+		url: '/Empleados-Agregar',
+		method: 'POST',
+		data: {
+			pnombre: pNombre.val(),
+			snombre: sNombre.val(),
+			papellido: pApellido.val(),
+			sapellido: sApellido.val(),
+			cedula: cedula.val(),
+			nacionalidad: nacionalidad.val(),
+			telefono: telefono.val(),
+			genero: genero.val(),
+			fnac: fnac.val() 
+		},
+		success: function(response){
+			if(response == 'great'){
+				alert('El empleado fue registrado satisfactoriamente');
+			}else{
+				alert('El empleado no se pudo agregar, revisa los campos');
+			}			
+		}
+	});
 });
 
-$('#passwordLogin').focus(function(){
-	$('#passwordLogin').removeClass('formsFieldError');
+$('#eliminarEmpleado').on('submit',function(e){
+	e.preventDefault();
+	let cedulaEmpleado = $('#cedulaEmpleadoEliminar');
+	let nacionalidadEmpleado = $('#nacionalidadEmpleadoEliminar');
+
+	$.ajax({
+		url: '/Empleados-Eliminar',
+		method: 'POST',
+		data: {
+			cedulaEmp: cedulaEmpleado.val(),
+			nacionalidadEmp: nacionalidadEmpleado.val()
+		},
+		success: function(response){
+			if(response == 'great'){
+				alert('El empleado fue ELIMINADO satisfactoriamente');
+			}else{
+				alert('El empleado NO SE PUDO ELIMINAR, revisa los campos');
+			}			
+		}
+	});
 });
 
+$('#verificarEmpleado').on('submit',function(e){
+	e.preventDefault();
+	let cedulaEmpleadoV = $('#cedulaEmpleadoVerificar');
+	let nacionalidadEmpleadoV = $('#nacionalidadEmpleadoVerificar');
 
+	$.ajax({
+		url: '/Empleados-Verificar',
+		method: 'POST',
+		data: {
+			cedulaEmpV: cedulaEmpleadoV.val(),
+			nacionalidadV: nacionalidadEmpleadoV.val()
+		},
+		success: function(response){
+			if(response[0].emp_cedula != null){
+				var boxModEmp = $('#guardarCambioEmpleado');
+
+				var fechaNac = new Date(response[0].emp_fechanacimiento);
+				var d = (fechaNac.getDate()).toString();
+				var m = (fechaNac.getMonth()+1).toString();
+				var y = fechaNac.getFullYear().toString();
+				console.log(d);
+				console.log(m);
+				console.log(y);
+				if( m < 10 ){
+					m = "0"+m;
+				};
+
+				if( d < 10 ){
+					d = "0"+d;
+				};
+
+				boxModEmp.html('');
+				boxModEmp.append(' \n\
+					<div class="animated" id="boxGC">\n\
+	                    <div class="form-row animated fadeIn animated fadeIn">\n\
+	                      <div class="col-md-6 mb-3">\n\
+	                        <label for="gcnombreEmpleado">Primer nombre</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gcnombreEmpleado" value="'+response[0].emp_nombre1+'" required>\n\
+	                      </div>\n\
+	                      <div class="col-md-6 mb-3">\n\
+	                        <label for="gcsegundoNombreEmpleado">Segundo nombre</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gcsegundoNombreEmpleado" value="'+response[0].emp_nombre2+'">\n\
+	                      </div>\n\
+	                    </div>\n\
+	                    <div class="form-row animated fadeIn">\n\
+	                      <div class="col-md-6 mb-3">\n\
+	                        <label for="gcapellidoEmpleado">Primer apellido</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gcapellidoEmpleado" value="'+response[0].emp_apellido1+'" required>\n\
+	                      </div>\n\
+	                      <div class="col-md-6 mb-3">\n\
+	                        <label for="gcsegundoApellidoEmpleado">Segundo apellido</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gcsegundoApellidoEmpleado" value="'+response[0].emp_apellido2+'">\n\
+	                      </div>\n\
+	                    </div>\n\
+	                    <hr>\n\
+	                    <div class="form-row animated fadeIn">\n\
+	                      <div class="col-md-2 mb-3">\n\
+	                        <label for="gcnacionalidadEmpleado">Nacionalidad</label>\n\
+	                        <select class="form-control formsCRUD" id="gcnacionalidadEmpleado" required>\n\
+	                          <option value="V">V</option>\n\
+	                          <option value="E">E</option>\n\
+	                        </select>\n\
+	                      </div>\n\
+	                      <div class="col-md-4 mb-3">\n\
+	                        <label for="gccedulaEmpleado">Cédula</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gccedulaEmpleado" value="'+response[0].emp_cedula+'" disabled>\n\
+	                      </div>\n\
+	                      <div class="col-md-6 mb-3">\n\
+	                        <label for="gcfechaNacimientoEmpleado">Fecha Nacimiento</label>\n\
+	                        <input type="date" class="form-control formsCRUD" id="gcfechaNacimientoEmpleado" value="'+y+'-'+m+'-'+d+'" required>\n\
+	                      </div>\n\
+	                    </div>\n\
+	                    <div class="form-row animated fadeIn">\n\
+	                      <div class="col-md-4 mb-3">\n\
+	                        <label for="gcgeneroEmpleado">Género</label>\n\
+	                        <select class="form-control formsCRUD" id="gcgeneroEmpleado" required>\n\
+	                          <option value="F">Femenino</option>\n\
+	                          <option value="M">Masculino</option>\n\
+	                          <option value="O">Otro</option>\n\
+	                        </select>\n\
+	                      </div>\n\
+	                      <div class="col-md-8 mb-3">\n\
+	                        <label for="gctelefonoEmpleado">Teléfono</label>\n\
+	                        <input type="text" class="form-control formsCRUD" id="gctelefonoEmpleado" value="'+ response[0].emp_telefono+'" required>\n\
+	                      </div>\n\
+	                    </div>\n\
+	                    <button class="btn btnForms btn-block animated fadeIn" type="submit">Guardar cambios</button>\n\
+	                </div>');
+					  $("#gcgeneroEmpleado option[value="+ response[0].emp_genero +"]").attr("selected",true);
+					  $("#gcnacionalidadEmpleado option[value="+ response[0].emp_nacionalidad +"]").attr("selected",true);
+			}else if(response == 'failed'){
+				alert('Error, no se consigue a empleado para modificar');				
+			}			
+		}
+	});
+});
 
 $('#formLogin').on('submit',function(e){
 	e.preventDefault();
 	let userLog = $('#userLogin');
 	let passwordLog = $('#passwordLogin');
-	userLogGlobal = userLog;
-	passwordLogGlobal =passwordLog;
 	$.ajax({
 		url: '/login',
 		method: 'POST',
@@ -26,7 +171,6 @@ $('#formLogin').on('submit',function(e){
 			if(response == 'access'){
 				window.location.href = "/Home";
 			}else{
-				createdemon = true;
 				userLog.addClass('formsFieldError');
 				passwordLog.addClass('formsFieldError');
 			}			
@@ -34,7 +178,53 @@ $('#formLogin').on('submit',function(e){
 	});
 });
 
-//Home
+$('#guardarCambioEmpleado').on('submit',function(e){
+	e.preventDefault();
+	let pNombreGC = $('#gcnombreEmpleado');
+	let sNombreGC = $('#gcsegundoNombreEmpleado');
+	let pApellidoGC = $('#gcapellidoEmpleado');
+	let sApellidoGC = $('#gcsegundoApellidoEmpleado');
+	let cedulaGC = $('#gccedulaEmpleado');
+	let nacionalidadMGC = $('#gcnacionalidadEmpleado');
+	let telefonoGC = $('#gctelefonoEmpleado');
+	let generoGC = $('#gcgeneroEmpleado');
+	let fnacGC = $('#gcfechaNacimientoEmpleado');
+
+	$.ajax({
+		url: '/Empleados-Modificar',
+		method: 'POST',
+		data: {
+			pnombreGC: pNombreGC.val(),
+			snombreGC: sNombreGC.val(),
+			papellidoGC: pApellidoGC.val(),
+			sapellidoGC: sApellidoGC.val(),
+			cedulaGC: cedulaGC.val(),
+			nacionalidadGC: nacionalidadMGC.val(),
+			telefonoGC: telefonoGC.val(),
+			generoGC: generoGC.val(),
+			fnacGC: fnacGC.val() 
+		},
+		success: function(response){
+			if(response == 'great'){
+				alert('El empleado fue MODIFICADO satisfactoriamente');
+				var boxGCEmp = $('#boxGC');
+				boxGCEmp.addClass('fadeOut');
+				boxGCEmp.html('');
+				var boxModEmp = $('#guardarCambioEmpleado');
+				boxModEmp.append(`\n\
+                    <div class="boxPrevMod animated fadeIn">\n\
+                      <h4 class="text-center">Selecciona un usuario para modificar su ficha</h4>\n\
+                      <p class="text-center"><i class="fas fa-address-card iconMod"></i></p>\n\
+                    </div>\n\
+				`);
+			}else{
+				alert('El empleado NO SE PUDO MOFICAR, revisa los campos');
+			}			
+		}
+	});
+});
+
+//MOVIMIENTOS DE RUTA
 
 $('#menuItemAliados').on('click',function(){
 	window.location.href = "/Home";
@@ -92,6 +282,22 @@ $('#menuItemHome').on('click',function(){
 	window.location.href = "/Home";
 });
 
+$('#menuItemAgregarEmpleado').on('click',function(){
+	window.location.href = "/Empleados-Agregar";
+});
+
+$('#menuItemConsultarEmpleado').on('click',function(){
+	window.location.href = "/Empleados-Consultar";
+});
+
+$('#menuItemEliminarEmpleado').on('click',function(){
+	window.location.href = "/Empleados-Eliminar";
+});
+
+$('#menuItemModificarEmpleado').on('click',function(){
+	window.location.href = "/Empleados-Modificar";
+});
+
 $('#backToHome').on('click',function(){
 	window.location.href = "/Home";
 });
@@ -100,6 +306,58 @@ $('#backToPersonal').on('click',function(){
 	window.location.href = "/Personal";
 });
 
+//Data Table
+$(document).ready( function () {
+    $('#table_id').DataTable();
+} );
+
+//ESTILOS
+$('#userLogin').focus(function(){
+	$('#userLogin').removeClass('formsFieldError');
+});
+
+$('#passwordLogin').focus(function(){
+	$('#passwordLogin').removeClass('formsFieldError');
+});
+
+
+/*
+
+$('#guardarCambioEmpleado').on('submit',function(e){
+	e.preventDefault();
+	let pNombreGC = $('#gcnombreEmpleado');
+	let sNombreGC = $('#gcsegundoNombreEmpleado');
+	let pApellidoGC = $('#gcapellidoEmpleado');
+	let sApellidoGC = $('#gcsegundoApellidoEmpleado');
+	let cedulaGC = $('#gccedulaEmpleado');
+	let nacionalidadGC = $('#gcnacionalidadEmpleado');
+	let telefonoGC = $('#gctelefonoEmpleado');
+	let generoGC = $('#gcgeneroEmpleado');
+	let fnacGC = $('#gcfechaNacimientoEmpleado');
+
+	$.ajax({
+		url: '/Empleados-Modificar',
+		method: 'POST',
+		data: {
+			pnombre: pNombreGC.val(),
+			snombre: sNombreGC.val(),
+			papellido: pApellidoGC.val(),
+			sapellido: sApellidoGC.val(),
+			cedula: cedulaGC.val(),
+			nacionalidad: nacionalidadGC.val(),
+			telefono: telefonoGC.val(),
+			genero: generoGC.val(),
+			fnac: fnacGC.val() 
+		},
+		success: function(response){
+			if(response == 'failedTest'){
+				alert('El empleado fue MODIFICADO satisfactoriamente');
+			}else{
+				alert('El empleado NO SE PUDO MOFICAR, revisa los campos');
+			}			
+		}
+	});
+});
 
 
 
@@ -107,26 +365,4 @@ $('#backToPersonal').on('click',function(){
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-function showErrorColumLog(){
-	let errorColumLog = $('#errorColumLog');
-
-	errorColumLog.html('');
-	errorColumLog.append(`
-		<p class="text-center textErrorUp"><i class="far fa-arrow-alt-circle-left"></i>  Error en usuario</p>
-		<p class="text-center textErrorDown"><i class="far fa-arrow-alt-circle-left"></i> Error en password</p>
-	`)
-};
-
+*/
