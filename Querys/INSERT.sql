93a5fd12d9942edd5943a70467a3328f00328b0e
David Monroy <djmonroy9@gmail.com>
2019-07-07 09:38:13 -0400

    aliados con todos min_min

--- Querys/INSERT.sql
+++ Querys/INSERT.sql
@@ -2388,7 +2388,13 @@ INSERT INTO lugar(
 
 -- ALIADOS CON MIN_MIN
 
-		INSERT INTO ali_mm (fk_amm_aliado,fk_amm_mm) VALUES ('G-200047062',1),('G-201109229',2),('G-205556895',6),('G-205965779',3),('G-205965789',1),('G-222265335',3),('G-202055844',1),('G-245944489',2);
+		INSERT INTO ali_mm (fk_amm_aliado,fk_amm_mm) VALUES (1,1),(2,2),(3,6),(4,3),(5,1),(6,3),(7,1),(8,2);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (11,11),(12,12),(13,13),(14,14),(15,15),(16,16),(17,17),(18,18),(19,19),(20,20);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (21,21),(22,22),(23,23),(24,24),(25,25),(26,26),(27,27),(28,28),(29,29),(30,30);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (31,31),(32,32),(33,33),(34,34),(35,35),(36,36),(37,37),(38,38),(39,39),(40,40);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (41,41),(42,42),(43,43),(44,44),(45,45),(46,46),(47,47),(48,48),(49,49),(50,50);
+		INSERT INTO ALI_MM (fk_amm_aliado,fk_amm_mm) VALUES (51,51);
 
 INSERT INTO yac_min(
 		ym_codigo, fk_ym_yacimiento, fk_ym_minmetalico, fk_ym_minnometalico, ym_cantidad)
